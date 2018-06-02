fe3bf813291798a04efd883274bd57968f3e2fbc
Alexandre Ximenes <xyymenes@gmail.com>
2018-06-02 10:53:45 -0300

    criando inputs de cadastro

--- appFotos/client/app/cadastro/cadastro.component.ts
+++ appFotos/client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FotoComponent } from "../foto/foto.component";
 
 @Component({
     moduleId: module.id,
@@ -7,4 +8,18 @@ import { Component } from "@angular/core";
 
 })
 
-export class CadastroComponent{}
+export class CadastroComponent{
+
+    foto: FotoComponent = new FotoComponent();
+
+    constructor(){
+        this.foto.descricao = " ";
+        this.foto.titulo = " ";
+        this.foto.url = " ";
+    }
+    cadastrar(event){
+        event.preventDefault();
+        
+        console.log(this.foto);
+    }
+}
